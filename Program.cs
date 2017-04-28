92b333671ca8887630a8c88a62d967730a93947b
Vladan68 <kohne68@gmail.com>
2017-04-28 10:35:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prvi_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Console.WriteLine("Zdravo žvete");  */
+
+            int x;
+            int y;
+            int z;
+            
+
+            x = 7;
+            y = x - 3;
+            z = x + y;
+
+            Console.WriteLine(z);
+            Console.ReadLine();
+
+        }
+    }
+}
